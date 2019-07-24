e7c0be0166feff459c34312cfdbacec466c8014e
caixioguangm <caixioguang110@gmail.com>
2019-07-24 16:48:43 +0800

    fix nav bug

--- main.js
+++ main.js
@@ -35,6 +35,8 @@ function initViewer() {
         baseLayerPicker: false,
         imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
             url: "http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=1902c209c7a7480dfb962751b839b91e",
+
+            // url:"http://10.150.25.19:3001/MapService.ashx?REQUEST=GetMap&SERVICE=CacheMap&Y={TileCol}&X={TileRow}&LEVEL={TileMatrix}&LAYERS=HN_Image&SessionID=0",
             layer: "tdtBasicLayer",
             style: "default",
             format: "image/jpeg",
@@ -292,58 +294,6 @@ function addFeature(e) {
     }
     viewer.dataSources.add(dataSource)
 
-            //     points.add({
-            //         id: point.NAME,
-            //         position: Cesium.Cartesian3.fromDegrees(lng, lat),
-            //         color: Cesium.Color.ORANGE,
-            //         outlineColor: Cesium.Color.WHITE,
-            //         outlineWidth: 0.2,
-            //         pixelSize: 8,
-            //         distanceDisplayCondition: new Cesium.DistanceDisplayCondition(1e4)
-            //     })
-            // }
-            // billboards.add({
-            //     position: Cesium.Cartesian3.fromDegrees(lng, lat),
-            //     color: new Cesium.Color.fromCssColorString("#3388ff"),
-            //     scaleByDistance: new Cesium.NearFarScalar(150, 1, 8e6, .2),
-            //     // image: drawCanvas( point.NAME, 18),
-            //     distanceDisplayCondition: new Cesium.DistanceDisplayCondition(0, 1e4),
-            // })
-
-
-            //datasource格式添加
-            dataSource.entities.add({
-                name: point.NAME,
-                position: Cesium.Cartesian3.fromDegrees(lng, lat),
-                point: {
-                    color: new Cesium.Color.fromCssColorString("#3388ff"),
-                    pixelSize: 10,
-                    outlineColor: new Cesium.Color.fromCssColorString("#ffffff"),
-                    outlineWidth: 1,
-                    heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-                    scaleByDistance: new Cesium.NearFarScalar(150, 1, 8e6, .2)
-                },
-                label: {
-                    text: point.NAME,
-                    font: "normal small-caps normal 17px 楷体",
-                    style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-                    fillColor: Cesium.Color.AZURE,
-                    outlineColor: Cesium.Color.BLACK,
-                    outlineWidth: 2,
-                    horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
-                    verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-                    pixelOffset: new Cesium.Cartesian2(0, -20),
-                    heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-                    distanceDisplayCondition: new Cesium.DistanceDisplayCondition(0, 1e4)
-                },
-                tooltip: {
-                    html: 'aa',
-                    anchor: [0, -12]
-                }
-            })
-        }
-    }
-    viewer.dataSources.add(dataSource)
     return
 
 
